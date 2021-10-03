41343ff2d64d4577e14b217a1b48235c044eac0d
JordanHayen <jordan.hayen@gmail.com>
2021-10-03 11:26:30 -0700

    Contains all files and screenshots for this activity

---
+++ Activity4/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Activity4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(487, 149);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 0;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(209, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter time in seconds:";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(381, 149);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.InputTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(209, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Result:";
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.OutputLabel.Location = new System.Drawing.Point(274, 182);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(82, 20);
+            this.OutputLabel.TabIndex = 4;
+            this.OutputLabel.Text = "0 seconds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Calculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label OutputLabel;
+    }
+}
+
---
+++ Activity4/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // this code runs when the Calculate button is clicked
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            double seconds; // this variable will store the input in seconds from the InputTextBox after it has been parsed into a numeric data type
+            double result = 0; // this variable will store the result of converting the input into another unit of time
+            string unit = "seconds"; // this varaible will store the unit of time to which the input has been converted
+            string output; // this variable will store the result variable and the unit formatted into a single string
+
+            if (Double.TryParse(InputTextBox.Text, out seconds)) // parses the text in the InputTextBox into a double and stores it in the result variable. If the parse is successful, a conditional statement runs to calculate the result
+            {
+                if (seconds >= 86400) // if the input is greater than 86400, it is divided by 86400 to yield a result in days
+                {
+                    unit = "days"; // sets the unit to days
+                    result = seconds / 86400; // sets the result to the input divided by 86400
+                }
+                else if (seconds >= 3600) // if the input is greater than 3600, it is divided by 3600 to yield a result in hours
+                {
+                    unit = "hours"; // sets the unit to hours
+                    result = seconds / 3600; // sets the result to the input divided by 3600
+                }
+                else if (seconds >= 60) // if the input is greater than 60, it is divided by 60 to yield a result in minutes
+                {
+                    unit = "minutes"; // sets the unit to minutes
+                    result = seconds / 60; // sets the result to the input divided by 60
+                }
+                else // if the result is less than 60, no division is required and the result will be in seconds
+                {
+                    unit = "seconds"; // sets the unit to seconds
+                    result = seconds; // sets the result to be equal to the input
+                }
+            }
+            else // if the parse is not successful, a message box is displayed requested a numeric input. Made with help from the textbook (Gaddis, 238)
+            {
+                MessageBox.Show("Please input a number."); // displays a message box requesting a numeric input
+            }
+
+            output = result.ToString() + " " + unit; // formats the result and the unit into one string
+            OutputLabel.Text = output; // sets the text of the OutputLabel to the formatted string
+        }
+    }
+}
+
+// References:
+// Gaddis, T. (2020). Starting Out With Visual C#. Pearson Education, Inc.
